ba8c2973978bad3e4b00e1eb50acc5b5af36b8f0
gehao <Sakura.gehao@gmail.com>
2021-06-14 22:51:17 +0800

    add echart template

---
+++ iot-vue/src/components/OnlineStatus.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="com-container">
+        <div class="com-chart" ref="online_status_ref"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            chartInstance: null,
+            allData: null,
+        }
+    },
+    mounted() {
+        this.initChart()
+        this.getData()
+        window.addEventListener('resize', this.screenAdapter)
+        this.screenAdapter()
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.screenAdapter)
+    },
+    methods: {
+        initChart() {
+            this.chartInstance = this.$echarts.init(this.$refs.online_status_ref)
+            const initOption = {}
+            this.chartInstance.setOption(initOption)
+        },
+        async getData() {
+            // this.allData 
+            this.updateChart()
+        },
+        updateChart() {
+            // 处理数据
+            const dataOption = {}
+            this.chartInstance.setOption(dataOption)
+        },
+        screenAdapter() {
+            const adapterOption = {}
+            this.chartInstance.setOption(adapterOption)
+            this.chartInstance.resize();
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ iot-vue/src/components/OnlineStatusPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="com-page">
+        <online-status />
+    </div>
+</template>
+
+<script>
+import OnlineStatus from '@/components/OnlineStatus'
+export default {
+    data() {
+        return {}
+    },
+    methods: {},
+    components: {
+        'online-status': OnlineStatus
+    }
+}
+</script>
+
+<style>
+
+</style>
