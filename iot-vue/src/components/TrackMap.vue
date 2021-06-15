4ba732bc15032648de65dc0279b1c7788e63a1e6
gehao <Sakura.gehao@gmail.com>
2021-06-15 10:58:52 +0800

    use baidu map api

---
+++ iot-vue/src/components/TrackMap.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="com-container">
+        <div class="com-chart" ref="track_map_ref">test</div>
+    </div>
+</template>
+
+<script>
+import 'echarts/dist/extension/bmap.min.js'
+export default {
+    data() {
+        return {
+            chartInstance: null
+        }
+    },
+    mounted() {
+        this.initChart()
+        this.getData()
+        window.addEventListener(this.screenAdapter)
+        this.screenAdapter()
+    },
+    destroyed() {
+        window.removeEventListener(this.screenAdapter)
+    },
+    methods: {
+        initChart() {
+            this.chartInstance = this.$echarts.init(this.$refs.track_map_ref)
+            
+            const initOption = {
+                bmap: {
+                    center: [113.844038, 22.907044],//默认中心点;
+                    zoom: 12,//缩放级别；
+                    roam: false,//是否缩放
+                    mapStyle:{style:'hardedge'},
+                    mapOptions: {
+                        enableMapClick: true//能都是地图可以点击
+                    },     
+                },
+                geo: {                
+                    map: 'bmap',                    
+                    type: 'map'
+                },
+                series: []    
+            }
+            this.chartInstance.setOption(initOption)
+        },
+        getData() {
+
+        },
+        updateChart() {
+
+        },
+        screenAdapter() {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ iot-vue/src/components/TrackMapPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <track-map></track-map>
+</template>
+
+<script>
+import TrackMap from '@/components/TrackMap'
+export default {
+    data() {
+        return {}
+    },
+    methods: {},
+    components: {
+        'track-map': TrackMap
+    }
+}
+</script>
+
+<style>
+
+</style>
