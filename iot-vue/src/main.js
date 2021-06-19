ad9c62b8a825e32a51ceecb4ac9b71420006a881
gehao <Sakura.gehao@gmail.com>
2021-06-19 10:39:14 +0800

    add main page

--- iot-vue/src/main.js
+++ iot-vue/src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 // 引入全局样式文件
 import './assets/css/global.less'
 // 引入图表自定义主题
--- iot-vue/src/router/index.js
+++ iot-vue/src/router/index.js
@@ -3,11 +3,20 @@ import Router from 'vue-router'
 import DataInfoPage from '@/components/DataInfoPage'
 import OnlineStatusPage from '@/components/OnlineStatusPage'
 import TrackMapPage from '@/components/TrackMapPage'
+import ScreenPage from '@/components/ScreenPage'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+        path: '/',
+        redirect: '/screen'
+    },
+    {
+        path: '/screen',
+        component: ScreenPage
+    },
     {
       path: '/datainfo',
       component: DataInfoPage
