72276653f41fe9ce058f4cfec15092a4957ef712
gehao <Sakura.gehao@gmail.com>
2021-06-21 14:43:11 +0800

    add drawer menuBar

---
+++ iot-vue/src/components/MenuBar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <el-button
+      type="menu"
+      icon="el-icon-s-unfold"
+      @click="drawer = true"
+      circle
+    ></el-button>
+    <el-drawer :visible.sync="drawer" direction="ltr">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+    </el-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+
+<style lang="css">
+.el-button--menu.is-active,
+.el-button--menu:active {
+  background: #20b2aa;
+  border-color: #20b2aa;
+  color: #fff;
+}
+
+.el-button--menu:focus,
+.el-button--menu:hover {
+  background: #48d1cc;
+  border-color: #48d1cc;
+  color: #fff;
+}
+
+.el-button--menu {
+  color: #fff;
+  background-color: #81e0dc;
+  border-color: #81e0dc;
+}
+</style>
