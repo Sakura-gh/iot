cc41e043d4086f2072457b5f1dd5a4bff3dde223
gehao <Sakura.gehao@gmail.com>
2021-06-25 15:02:14 +0800

    move docker compose files to docker/

---
+++ docker/mysql/init/iot.sql
@@ -0,0 +1,19 @@
+create database if not exists iot;
+use iot;
+create table if not exists iot_message (
+	client_id		varchar(32),
+	info 			varchar(100),
+	value			int,
+	alert			int,
+	lng				double,
+	lat				double,
+	timestamp		bigint,
+	primary key(client_id, timestamp)
+);
+
+create table if not exists user (
+	username		varchar(32),
+	password		varchar(32),
+	email			varchar(64),
+	primary key(username)
+);
